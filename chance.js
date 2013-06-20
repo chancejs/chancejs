21c3fb9fa3e907ea084aa52a83ca91690c29b239
Victor Quinn <vquinn@ngpvan.com>
2013-06-20 17:17:46 -0400

    Initial commit of chance.js

---
+++ chance.js
@@ -0,0 +1,39 @@
+//  Chance.js 0.1
+//  http://chancejs.com
+//  (c) 2013 Victor Quinn
+//  Chance may be freely distributed or modified under the MIT license.
+
+(function() {
+
+    var chance = {
+        VERSION: 0.1,
+
+        str: function (length) {
+            var text = "";
+            var possible = "abcdefghijklmnopqrstuvwxyz";
+
+            for (var i = 0; i < length; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+
+            return text;
+        }
+    };
+
+
+    // CommonJS module
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = chance;
+        }
+        exports.chance = chance;
+    }
+
+    // Register as a named AMD module
+    if (typeof define === 'function' && define.amd) {
+        define('chance', [], function() {
+            return chance;
+        });
+    }
+
+})();
