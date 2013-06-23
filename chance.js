e2e467379b73c8abe1831a7f696d039406677bf3
Victor Quinn <vquinn@ngpvan.com>
2013-06-22 23:27:30 -0400

    Add state to docs, latest chance.js

--- chance.js
+++ chance.js
@@ -104,6 +104,80 @@
         return suffix;
     };
 
+    Chance.prototype.states = function () {
+        return [
+            { name: "Alabama", abbreviation: "AL" },
+            { name: "Alaska", abbreviation: "AK" },
+            { name: "American Samoa", abbreviation: "AS" },
+            { name: "Arizona", abbreviation: "AZ" },
+            { name: "Arkansas", abbreviation: "AR" },
+            { name: "Armed Forces Europe", abbreviation: "AE" },
+            { name: "Armed Forces Pacific", abbreviation: "AP" },
+            { name: "Armed Forces the Americas", abbreviation: "AA" },
+            { name: "California", abbreviation: "CA" },
+            { name: "Colorado", abbreviation: "CO" },
+            { name: "Connecticut", abbreviation: "CT" },
+            { name: "Delaware", abbreviation: "DE" },
+            { name: "District of Columbia", abbreviation: "DC" },
+            { name: "Federated States of Micronesia", abbreviation: "FM" },
+            { name: "Florida", abbreviation: "FL" },
+            { name: "Georgia", abbreviation: "GA" },
+            { name: "Guam", abbreviation: "GU" },
+            { name: "Hawaii", abbreviation: "HI" },
+            { name: "Idaho", abbreviation: "ID" },
+            { name: "Illinois", abbreviation: "IL" },
+            { name: "Indiana", abbreviation: "IN" },
+            { name: "Iowa", abbreviation: "IA" },
+            { name: "Kansas", abbreviation: "KS" },
+            { name: "Kentucky", abbreviation: "KY" },
+            { name: "Louisiana", abbreviation: "LA" },
+            { name: "Maine", abbreviation: "ME" },
+            { name: "Marshall Islands", abbreviation: "MH" },
+            { name: "Maryland", abbreviation: "MD" },
+            { name: "Massachusetts", abbreviation: "MA" },
+            { name: "Michigan", abbreviation: "MI" },
+            { name: "Minnesota", abbreviation: "MN" },
+            { name: "Mississippi", abbreviation: "MS" },
+            { name: "Missouri", abbreviation: "MO" },
+            { name: "Montana", abbreviation: "MT" },
+            { name: "Nebraska", abbreviation: "NE" },
+            { name: "Nevada", abbreviation: "NV" },
+            { name: "New Hampshire", abbreviation: "NH" },
+            { name: "New Jersey", abbreviation: "NJ" },
+            { name: "New Mexico", abbreviation: "NM" },
+            { name: "New York", abbreviation: "NY" },
+            { name: "North Carolina", abbreviation: "NC" },
+            { name: "North Dakota", abbreviation: "ND" },
+            { name: "Northern Mariana Islands", abbreviation: "MP" },
+            { name: "Ohio", abbreviation: "OH" },
+            { name: "Oklahoma", abbreviation: "OK" },
+            { name: "Oregon", abbreviation: "OR" },
+            { name: "Pennsylvania", abbreviation: "PA" },
+            { name: "Puerto Rico", abbreviation: "PR" },
+            { name: "Rhode Island", abbreviation: "RI" },
+            { name: "South Carolina", abbreviation: "SC" },
+            { name: "South Dakota", abbreviation: "SD" },
+            { name: "Tennessee", abbreviation: "TN" },
+            { name: "Texas", abbreviation: "TX" },
+            { name: "Utah", abbreviation: "UT" },
+            { name: "Vermont", abbreviation: "VT" },
+            { name: "Virgin Islands, U.S.", abbreviation: "VI" },
+            { name: "Virginia", abbreviation: "VA" },
+            { name: "Washington", abbreviation: "WA" },
+            { name: "West Virginia", abbreviation: "WV" },
+            { name: "Wisconsin", abbreviation: "WI" },
+            { name: "Wyoming", abbreviation: "WY" }
+        ];
+    };
+
+    Chance.prototype.state = function (options) {
+        var states = this.states(),
+            state = (options && options.full) ?
+              states[this.natural({max: states.length - 1})].name :
+              states[this.natural({max: states.length - 1})].abbreviation;
+
+        return state;
+    };
 
     // Dice - For all the board game geeks out there, myself included ;)
     Chance.prototype.d4 = function () { return this.natural({min: 1, max: 4}); };
