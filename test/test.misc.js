35ee27d16a73e0f15ee41ed8e77fc77b61328060
Victor Quinn <vquinn@ngpvan.com>
2013-06-23 11:21:50 -0400

    Add guid() method, test for that method, update dice tests to use within

--- test/test.misc.js
+++ test/test.misc.js
@@ -1,6 +1,5 @@
 define(['Chance', 'mocha', 'chai', 'underscore'], function (Chance, mocha, chai, _) {
-    var assert = chai.assert,
-        expect = chai.expect;
+    var expect = chai.expect;
 
     describe("Dice", function () {
         var die, chance = new Chance();
@@ -8,48 +7,53 @@ define(['Chance', 'mocha', 'chai', 'underscore'], function (Chance, mocha, chai,
         it("returns a properly bounded d4", function () {
             _(1000).times(function () {
                 die = chance.d4();
-                expect(die).to.be.above(0);
-                expect(die).to.be.below(5);
+                expect(die).to.be.within(1, 4);
             });
         });
 
         it("returns a properly bounded d6", function () {
             _(1000).times(function () {
                 die = chance.d6();
-                expect(die).to.be.above(0);
-                expect(die).to.be.below(7);
+                expect(die).to.be.within(1, 6);
             });
         });
 
         it("returns a properly bounded d8", function () {
             _(1000).times(function () {
                 die = chance.d8();
-                expect(die).to.be.above(0);
-                expect(die).to.be.below(9);
+                expect(die).to.be.within(1, 8);
             });
         });
 
         it("returns a properly bounded d10", function () {
             _(1000).times(function () {
                 die = chance.d10();
-                expect(die).to.be.above(0);
-                expect(die).to.be.below(11);
+                expect(die).to.be.within(1, 10);
             });
         });
 
         it("returns a properly bounded d12", function () {
             _(1000).times(function () {
                 die = chance.d12();
-                expect(die).to.be.above(0);
-                expect(die).to.be.below(13);
+                expect(die).to.be.within(1, 12);
             });
         });
 
         it("returns a properly bounded d20", function () {
             _(1000).times(function () {
                 die = chance.d20();
-                expect(die).to.be.above(0);
-                expect(die).to.be.below(21);
+                expect(die).to.be.within(1, 20);
+            });
+        });
+    });
+
+    describe("Guid", function () {
+        var guid, chance = new Chance();
+
+        it("returns a proper guid", function () {
+            _(1000).times(function () {
+                guid = chance.guid();
+                expect(guid).to.match(/([0-9a-fA-F]){8}(-([0-9a-fA-F]){4}){3}-([0-9a-fA-F]){12}/);
             });
         });
     });
