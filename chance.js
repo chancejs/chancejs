593303a2aa0b75cfde008b61434184c3f03f123c
Victor Quinn <vquinn@ngpvan.com>
2013-06-23 23:32:50 -0400

    Bump version numberls

--- chance.js
+++ chance.js
@@ -1,4 +1,4 @@
-//  Chance.js 0.2.2
+//  Chance.js 0.3.0
 //  http://chancejs.com
 //  (c) 2013 Victor Quinn
 //  Chance may be freely distributed or modified under the MIT license.
@@ -387,7 +387,7 @@
 
     // -- End Miscellaneous --
 
-    Chance.prototype.VERSION = "0.2.2";
+    Chance.prototype.VERSION = "0.3.0";
 
     // Mersenne Twister from https://gist.github.com/banksean/300494
     var MersenneTwister = function (seed) {
