a277f342d6ea7bcd35fe34f02e4c2aedb8ffba76
Victor Quinn <vquinn@ngpvan.com>
2013-07-01 10:40:56 -0400

    Add first, last, fix prefix bug for name

--- test/test.name.js
+++ test/test.name.js
@@ -2,7 +2,7 @@ define(['Chance', 'mocha', 'chai', 'underscore'], function (Chance, mocha, chai,
     var expect = chai.expect;
 
     describe("Name", function () {
-        var name, prefix, chance = new Chance();
+        var name, first, last, prefix, chance = new Chance();
 
         describe("name()", function () {
             it("returns a random name", function () {
@@ -29,6 +29,37 @@ define(['Chance', 'mocha', 'chai', 'underscore'], function (Chance, mocha, chai,
                     expect(name.split(' ')).to.have.length(3);
                     expect(name.split(' ')[1]).to.match(/[A-Z]\./);
                     expect(name.indexOf('.')).to.be.ok;
+                });
+            });
+            it("can have the prefix specified", function () {
+                _(1000).times(function () {
+                    name = chance.name({prefix: true});
+                    expect(name).to.be.a('string');
+                    expect(name.split(' ')).to.have.length(3);
+                });
+            });
+
+        });
+
+        describe("first()", function () {
+            it("returns a random first name", function () {
+                _(1000).times(function () {
+                    first = chance.first();
+                    expect(first).to.be.a('string');
+                    expect(first).to.have.length.within(2, 20);
+                    expect(first.split(' ')).to.have.length(1);
+                });
+            });
+
+        });
+
+        describe("last()", function () {
+            it("returns a random last name", function () {
+                _(1000).times(function () {
+                    last = chance.last();
+                    expect(last).to.be.a('string');
+                    expect(last).to.have.length.within(2, 20);
+                    expect(last.split(' ')).to.have.length(1);
                 });
             });
 
