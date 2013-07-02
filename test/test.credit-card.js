9e41e9982dfa5ddeebf711fe8b89460a236707a9
Tim Petricola <tim.petricola@gmail.com>
2013-07-02 15:35:52 -0700

    Fix credit-card test

--- test/test.credit-card.js
+++ test/test.credit-card.js
@@ -2,14 +2,14 @@ define(['Chance', 'mocha', 'chai', 'underscore'], function (Chance, mocha, chai,
     var expect = chai.expect;
 
     describe("Credit Card", function () {
-        var chance = new Chance();
+        var type, number, chance = new Chance();
 
         describe("Luhn Check", function () {
             it("checks if number passes Luhn algorithm", function () {
-                expect(chance.luhn_check(49927398716)).to.equal(true);
-                expect(chance.luhn_check(1234567812345670)).to.equal(true);
-                expect(chance.luhn_check(49927398717)).to.equal(false);
-                expect(chance.luhn_check(1234567812345678)).to.equal(false);
+                expect(chance.luhn_check(49927398716)).to.be.true;
+                expect(chance.luhn_check(1234567812345670)).to.be.true;
+                expect(chance.luhn_check(49927398717)).to.be.false;
+                expect(chance.luhn_check(1234567812345678)).to.be.false;
             });
         });
 
