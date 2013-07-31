chancejs/chancejs

commit 18b8f906afaeadb8cc3e091b36942967d2b00041

Simplified the fixed version of shuffle since splice returns the array item you are removing.

chance.js
@@ -206,10 +206,9 @@
         for (var i = 0; i < length; i++) {
             // Pick a random index from the array
             j = this.natural({max: arr.length - 1});
-            // Add it to the new array
-            new_array[i] = arr[j];
-            // Remove that element from the original array
-            arr.splice(j, 1);
+            // Remove it from the array and add it to the new array
+            new_array[i] = arr.splice(j, 1);
+            
         }
 
         return new_array;

commit d445feb0e79359d2cc7410d7cfe70c863aca73c4

Fix shuffle() broken by commit 18b8f906afaeadb8cc3e091b3, add test so it doesn't happen again

chance.js
@@ -211,9 +211,10 @@
         for (var i = 0; i < length; i++) {
             // Pick a random index from the array
             j = this.natural({max: arr.length - 1});
-            // Remove it from the array and add it to the new array
-            new_array[i] = arr.splice(j, 1);
-
+            // Add it to the new array
+            new_array[i] = arr[j];
+            // Remove that element from the original array
+            arr.splice(j, 1);
         }
 
         return new_array;

test/test.helpers.js
@@ -29,6 +29,7 @@ define(['Chance', 'mocha', 'chai', 'underscore'], function (Chance, mocha, chai,
                 arr = ['a', 'b', 'c', 'd', 'e'];
                 _(1000).times(function () {
                     expect(chance.shuffle(_.clone(arr))).to.have.length(5);
+                    expect(chance.shuffle(_.clone(arr))).to.contain('a');
                 });
             });
         });