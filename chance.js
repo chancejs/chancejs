6d5cbc7c5d21665e82213ede124e02fbe835b79c
Kevin Garnett <kgarnett24@gmail.com>
2013-08-07 23:39:25 -0300

    Valid GUIDs
    Generate valid variant 2 GUID with the ability to specify version
    number (1-5)

--- chance.js
+++ chance.js
@@ -915,12 +915,21 @@
     };
 
     // Guid
-    Chance.prototype.guid = function () {
-        return this.hash({casing: 'upper', length: 8}) + '-' +
-            this.hash({casing: 'upper', length: 4}) + '-' +
-            this.hash({casing: 'upper', length: 4}) + '-' +
-            this.hash({casing: 'upper', length: 4}) + '-' +
-            this.hash({casing: 'upper', length: 12});
+    Chance.prototype.guid = function (options) {
+        options = options || {version: 5};
+        
+        var guid_pool = "ABCDEF1234567890",
+            variant_pool = "AB89",
+            guid = this.string({pool: guid_pool, length: 8}) + '-' +
+                   this.string({pool: guid_pool, length: 4}) + '-' +
+                   // The Version
+                   options.version + 
+                   this.string({pool: guid_pool, length: 3}) + '-' +
+                   // The Variant
+                   this.string({pool: variant_pool, length: 1}) +
+                   this.string({pool: guid_pool, length: 3}) + '-' +
+                   this.string({pool: guid_pool, length: 12});
+        return guid;
     };
 
     // Hash
