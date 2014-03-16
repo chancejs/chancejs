06c5a0ee10e5a9dfa6334ce26fd4c167c636a102
Victor Quinn <victor@socialradar.com>
2014-03-16 17:07:30 -0400

    Bump version number

--- chance.js
+++ chance.js
@@ -1,4 +1,4 @@
-//  Chance.js 0.5.4
+//  Chance.js 0.5.5
 //  http://chancejs.com
 //  (c) 2013 Victor Quinn
 //  Chance may be freely distributed or modified under the MIT license.
@@ -1134,7 +1134,7 @@
 
     // -- End Miscellaneous --
 
-    Chance.prototype.VERSION = "0.5.4";
+    Chance.prototype.VERSION = "0.5.5";
 
     // Mersenne Twister from https://gist.github.com/banksean/300494
     var MersenneTwister = function (seed) {
