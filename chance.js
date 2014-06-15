3c9fe9d4167f361e3bac4965b016570f3ee7d14e
Alex DiLiberto <alex@alexdiliberto.com>
2014-06-15 17:43:50 -0400

    Breaking up states in default (US/DC), territores, and armed forces.

--- chance.js
+++ chance.js
@@ -687,29 +687,27 @@
 
     Chance.prototype.state = function (options) {
         return (options && options.full) ?
-            this.pick(this.states()).name :
-            this.pick(this.states()).abbreviation;
+            this.pick(this.states(options)).name :
+            this.pick(this.states(options)).abbreviation;
     };
 
-    Chance.prototype.states = function () {
-        return [
+    Chance.prototype.states = function (options) {
+        options = initOptions(options);
+
+        var states, us_states_and_dc, territories, armed_forces;
+
+        us_states_and_dc = [
             {name: 'Alabama', abbreviation: 'AL'},
             {name: 'Alaska', abbreviation: 'AK'},
-            {name: 'American Samoa', abbreviation: 'AS'},
             {name: 'Arizona', abbreviation: 'AZ'},
             {name: 'Arkansas', abbreviation: 'AR'},
-            {name: 'Armed Forces Europe', abbreviation: 'AE'},
-            {name: 'Armed Forces Pacific', abbreviation: 'AP'},
-            {name: 'Armed Forces the Americas', abbreviation: 'AA'},
             {name: 'California', abbreviation: 'CA'},
             {name: 'Colorado', abbreviation: 'CO'},
             {name: 'Connecticut', abbreviation: 'CT'},
             {name: 'Delaware', abbreviation: 'DE'},
             {name: 'District of Columbia', abbreviation: 'DC'},
-            {name: 'Federated States of Micronesia', abbreviation: 'FM'},
             {name: 'Florida', abbreviation: 'FL'},
             {name: 'Georgia', abbreviation: 'GA'},
-            {name: 'Guam', abbreviation: 'GU'},
             {name: 'Hawaii', abbreviation: 'HI'},
             {name: 'Idaho', abbreviation: 'ID'},
             {name: 'Illinois', abbreviation: 'IL'},
@@ -719,7 +717,6 @@
             {name: 'Kentucky', abbreviation: 'KY'},
             {name: 'Louisiana', abbreviation: 'LA'},
             {name: 'Maine', abbreviation: 'ME'},
-            {name: 'Marshall Islands', abbreviation: 'MH'},
             {name: 'Maryland', abbreviation: 'MD'},
             {name: 'Massachusetts', abbreviation: 'MA'},
             {name: 'Michigan', abbreviation: 'MI'},
@@ -735,12 +732,10 @@
             {name: 'New York', abbreviation: 'NY'},
             {name: 'North Carolina', abbreviation: 'NC'},
             {name: 'North Dakota', abbreviation: 'ND'},
-            {name: 'Northern Mariana Islands', abbreviation: 'MP'},
             {name: 'Ohio', abbreviation: 'OH'},
             {name: 'Oklahoma', abbreviation: 'OK'},
             {name: 'Oregon', abbreviation: 'OR'},
             {name: 'Pennsylvania', abbreviation: 'PA'},
-            {name: 'Puerto Rico', abbreviation: 'PR'},
             {name: 'Rhode Island', abbreviation: 'RI'},
             {name: 'South Carolina', abbreviation: 'SC'},
             {name: 'South Dakota', abbreviation: 'SD'},
@@ -748,13 +743,39 @@
             {name: 'Texas', abbreviation: 'TX'},
             {name: 'Utah', abbreviation: 'UT'},
             {name: 'Vermont', abbreviation: 'VT'},
-            {name: 'Virgin Islands, U.S.', abbreviation: 'VI'},
             {name: 'Virginia', abbreviation: 'VA'},
             {name: 'Washington', abbreviation: 'WA'},
             {name: 'West Virginia', abbreviation: 'WV'},
             {name: 'Wisconsin', abbreviation: 'WI'},
             {name: 'Wyoming', abbreviation: 'WY'}
         ];
+
+        territories = [
+            {name: 'American Samoa', abbreviation: 'AS'},
+            {name: 'Federated States of Micronesia', abbreviation: 'FM'},
+            {name: 'Guam', abbreviation: 'GU'},
+            {name: 'Marshall Islands', abbreviation: 'MH'},
+            {name: 'Northern Mariana Islands', abbreviation: 'MP'},
+            {name: 'Puerto Rico', abbreviation: 'PR'},
+            {name: 'Virgin Islands, U.S.', abbreviation: 'VI'}
+        ];
+
+        armed_forces = [
+            {name: 'Armed Forces Europe', abbreviation: 'AE'},
+            {name: 'Armed Forces Pacific', abbreviation: 'AP'},
+            {name: 'Armed Forces the Americas', abbreviation: 'AA'}
+        ];
+
+        states = us_states_and_dc;
+
+        if (options.territories) {
+            states = states.concat(territories);
+        }
+        if (options.armed_forces) {
+            states = states.concat(armed_forces);
+        }
+
+        return states;
     };
 
     Chance.prototype.street = function (options) {
