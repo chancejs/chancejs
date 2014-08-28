24148f8c8b197ad2114c80795f9fd24f601bd84a
Chris Villarreal <chrisvillarreal1018@gmail.com>
2014-08-28 11:08:58 -0500

    Added CPF generator for getting a taxpayer id for Brazil citizens. Basic test included.

--- chance.js
+++ chance.js
@@ -63,6 +63,15 @@
         }
     }
 
+    function gen_random(num){
+        var ranNum = Math.round(Math.random()*num);
+        return ranNum;
+    }
+
+    function mod(dividen,divisor) {
+        return Math.round(dividen - (Math.floor(dividen/divisor)*divisor));
+    }
+
     // -- Basics --
 
     Chance.prototype.bool = function (options) {
@@ -510,6 +519,33 @@
         }
         return ssn;
     };
+
+    // CPF; ID to identify taxpayers in Brazil
+    Chance.prototype.cpf = function (){
+        var n = 9;
+        var n1 = gen_random(n);
+        var n2 = gen_random(n);
+        var n3 = gen_random(n);
+        var n4 = gen_random(n);
+        var n5 = gen_random(n);
+        var n6 = gen_random(n);
+        var n7 = gen_random(n);
+        var n8 = gen_random(n);
+        var n9 = gen_random(n);
+        var d1 = n9*2+n8*3+n7*4+n6*5+n5*6+n4*7+n3*8+n2*9+n1*10;
+        d1 = 11 - ( mod(d1,11) );
+        if (d1>=10){
+            d1 = 0;
+        }
+        var d2 = d1*2+n9*3+n8*4+n7*5+n6*6+n5*7+n4*8+n3*9+n2*10+n1*11;
+        d2 = 11 - ( mod(d2,11) );
+        if (d2>=10){
+            d2 = 0;
+        }
+        result = '';
+        result = ''+n1+n2+n3+'.'+n4+n5+n6+'.'+n7+n8+n9+'-'+d1+d2;
+        return result;
+    }
 
     // -- End Person --
 
