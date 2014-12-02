b4cd2feeb4e3dce9f7342b40ea3aa9d3ab2fc882
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-12-02 09:16:48 +0100

    Added BlackBerry device PIN

--- chance.js
+++ chance.js
@@ -559,10 +559,16 @@
         return this.string({ pool: "abcdef1234567890", length: 64 });
     };
 
-    //Windows Phone 7 ANID
+    // Windows Phone 7 ANID
     Chance.prototype.wp7_anid = function (options) {
         return 'A=' + this.guid().replace(/-/g, '').toUpperCase() + '&E=' + this.hash({ length:3 }) + '&W=' + this.integer({ min:0, max:9 });
     };
+
+    // BlackBerry Device PIN
+    Chance.prototype.bb_pin = function (options) {
+        return this.hash({ length: 8 });
+    };
+
     // -- End Mobile --
 
     // -- Web --
