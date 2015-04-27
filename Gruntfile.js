c68a450c92bfb28d4e048e925a4e386678ecb014
Victor Quinn <victor@socialradar.com>
2015-04-27 18:39:36 -0400

    Simplify grunt ci script

--- Gruntfile.js
+++ Gruntfile.js
@@ -91,4 +91,5 @@ module.exports = function (grunt) {
 
     grunt.registerTask('default', ['watch']);
     grunt.registerTask('test', ['js-test', 'shell']);
+    grunt.registerTask('test-ci', ['jshint', 'js-test', 'shell']);
 };
