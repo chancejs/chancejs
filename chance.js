f8d8e9c29149b8c882a78e480677182e4eb27469
Victor Quinn <victor@socialradar.com>
2015-09-11 02:23:51 -0400

    Remove Chance CLI, now in its own repo at https://github.com/victorquinn/chancejs-cli, bump version due to possible breaking change

--- chance.js
+++ chance.js
@@ -1,4 +1,4 @@
-//  Chance.js 0.7.7
+//  Chance.js 0.8.0
 //  http://chancejs.com
 //  (c) 2013 Victor Quinn
 //  Chance may be freely distributed or modified under the MIT license.
@@ -62,7 +62,7 @@
         return this;
     }
 
-    Chance.prototype.VERSION = "0.7.7";
+    Chance.prototype.VERSION = "0.8.0";
 
     // Random helper functions
     function initOptions(options, defaults) {
