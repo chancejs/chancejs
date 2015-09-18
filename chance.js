8a34604d36628c2cc130cb5995934b2fa333e48f
The Usual Coder <theusualcoder@gmail.com>
2015-09-18 16:37:45 +0900

    Return array when count

--- chance.js
+++ chance.js
@@ -368,10 +368,17 @@
     };
 
     Chance.prototype.pick = function (arr, count) {
+        if (count === 0) {
+            return [];
+        }
         if (arr.length === 0) {
             throw new RangeError("Chance: Cannot pick() from an empty array");
         }
-        if (!count || count === 1) {
+        if (count < 0) {
+            throw new RangeError("Chance: count must be positive");
+        }
+        
+        if (!count) {
             return arr[this.natural({max: arr.length - 1})];
         } else {
             return this.shuffle(arr).slice(0, count);
