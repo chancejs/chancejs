46c64394e489b253df7d8c91b2ab6748aad15aab
Matteo Fogli <matteo@modo.md>
2016-03-30 11:15:21 +0200

    support internationalisation on chance.state(s)
    Fully backward compatible
    Since U.S. territories were semantically partitioned, a new object has been created
    (country_regions) following the same structure as other country based data
    (each country has a property that contains an array of data)
    Source for data: ISTAT (public government body for statistical analysis)

--- chance.js
+++ chance.js
@@ -1326,23 +1326,30 @@
     };
 
     Chance.prototype.states = function (options) {
-        options = initOptions(options, { us_states_and_dc: true });
+        options = initOptions(options, { country: 'us', us_states_and_dc: true } );
 
-        var states,
-            us_states_and_dc = this.get("us_states_and_dc"),
-            territories = this.get("territories"),
-            armed_forces = this.get("armed_forces");
+        var states;
 
-        states = [];
+        switch (options.country.toLowerCase()) {
+            case 'us':
+                var us_states_and_dc = this.get("us_states_and_dc"),
+                    territories = this.get("territories"),
+                    armed_forces = this.get("armed_forces");
 
-        if (options.us_states_and_dc) {
-            states = states.concat(us_states_and_dc);
-        }
-        if (options.territories) {
-            states = states.concat(territories);
-        }
-        if (options.armed_forces) {
-            states = states.concat(armed_forces);
+                states = [];
+
+                if (options.us_states_and_dc) {
+                    states = states.concat(us_states_and_dc);
+                }
+                if (options.territories) {
+                    states = states.concat(territories);
+                }
+                if (options.armed_forces) {
+                    states = states.concat(armed_forces);
+                }
+                break;
+            case 'it':
+                states = this.get("country_regions")[options.country.toLowerCase()];
         }
 
         return states;
@@ -2361,6 +2368,31 @@
             {name: 'Armed Forces Pacific', abbreviation: 'AP'},
             {name: 'Armed Forces the Americas', abbreviation: 'AA'}
         ],
+
+        country_regions: {
+            it: [
+                { name: "Valle d'Aosta", abbreviation: "VDA" },
+                { name: "Piemonte", abbreviation: "PIE" },
+                { name: "Lombardia", abbreviation: "LOM" },
+                { name: "Veneto", abbreviation: "VEN" },
+                { name: "Trentino Alto Adige", abbreviation: "TAA" },
+                { name: "Friuli Venezia Giulia", abbreviation: "FVG" },
+                { name: "Liguria", abbreviation: "LIG" },
+                { name: "Emilia Romagna", abbreviation: "EMR" },
+                { name: "Toscana", abbreviation: "TOS" },
+                { name: "Umbria", abbreviation: "UMB" },
+                { name: "Marche", abbreviation: "MAR" },
+                { name: "Abruzzo", abbreviation: "ABR" },
+                { name: "Lazio", abbreviation: "LAZ" },
+                { name: "Campania", abbreviation: "CAM" },
+                { name: "Puglia", abbreviation: "PUG" },
+                { name: "Basilicata", abbreviation: "BAS" },
+                { name: "Molise", abbreviation: "MOL" },
+                { name: "Calabria", abbreviation: "CAL" },
+                { name: "Sicilia", abbreviation: "SIC" },
+                { name: "Sardegna", abbreviation: "SAR" }
+            ]
+        },
 
         street_suffixes: [
             {name: 'Avenue', abbreviation: 'Ave'},
