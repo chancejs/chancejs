e1e9b311afb7822aba2452e77b85afa75d821fff
Matteo Fogli <matteo@modo.md>
2016-03-31 01:48:07 +0200

    add generator for Italian Codice Fiscale (SSN)

--- chance.js
+++ chance.js
@@ -1704,6 +1704,89 @@
         return it_vat + this.luhn_calculate(it_vat);
     };
 
+    /*
+     * this generator is written following the official algorithm
+     * all data can be passed explicitely or randomized by calling chance.cf() without options
+     * the code does not check that the input data is valid (it goes beyond the scope of the generator)
+     *
+     * @param  [Object] options = { first: first name,
+     *                              last: last name,
+     *                              gender: female|male,
+                                    birthday: JavaScript date object,
+                                    city: string(4), 1 letter + 3 numbers
+                                   }
+     * @return [string] codice fiscale
+     *
+    */
+    Chance.prototype.cf = function (options) {
+        options = options || {};
+        var gender = !!options.gender ? options.gender : this.gender(),
+            first = !!options.first ? options.first : this.first( { gender: gender, nationality: 'it'} ),
+            last = !!options.last ? options.last : this.last( { nationality: 'it'} ),
+            birthday = !!options.birthday ? options.birthday : this.birthday(),
+            city = !!options.city ? options.city : this.pickone(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'L', 'M', 'Z']) + this.pad(this.natural({max:999}), 3),
+            cf = [],
+            name_generator = function(name, isLast) {
+                var temp,
+                    return_value = [];
+
+                if (name.length < 3) {
+                    return_value = name.split("").concat("XXX".split("")).splice(0,3);
+                }
+                else {
+                    temp = name.toUpperCase().split('').map(function(c){
+                        return ("BCDFGHJKLMNPRSTVWZ".indexOf(c) !== -1) ? c : undefined;
+                    }).join('');
+                    if (temp.length > 3) {
+                        if (isLast) {
+                            temp = temp.substr(0,3);
+                        } else {                        
+                            temp = temp[0] + temp.substr(2,2);
+                        }
+                    }
+                    if (temp.length < 3) {
+                        return_value = temp;
+                        temp = name.toUpperCase().split('').map(function(c){
+                            return ("AEIOU".indexOf(c) !== -1) ? c : undefined;
+                        }).join('').substr(0, 3 - return_value.length);
+                    }
+                    return_value = return_value + temp;
+                }
+
+                return return_value;
+            },
+            date_generator = function(birthday, gender, that) {
+                var lettermonths = ['A', 'B', 'C', 'D', 'E', 'H', 'L', 'M', 'P', 'R', 'S', 'T'];
+
+                return  birthday.getFullYear().toString().substr(2) + 
+                        lettermonths[birthday.getMonth()] +
+                        that.pad(birthday.getDate() + ((gender.toLowerCase() === "female") ? 40 : 0), 2);
+            },
+            checkdigit_generator = function(cf) {
+                var range1 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                    range2 = "ABCDEFGHIJABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                    evens  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                    odds   = "BAKPLCQDREVOSFTGUHMINJWZYX",
+                    digit  = 0;
+
+
+                for(var i = 0; i < 15; i++) {
+                    if (i % 2 !== 0) {
+                        digit += evens.indexOf(range2[range1.indexOf(cf[i])]);
+                    }
+                    else {
+                        digit +=  odds.indexOf(range2[range1.indexOf(cf[i])]);
+                    }
+                }
+                return evens[digit % 26];
+            };
+
+        cf = cf.concat(name_generator(last, true), name_generator(first), date_generator(birthday, gender, this), city.toUpperCase().split("")).join("");
+        cf += checkdigit_generator(cf.toUpperCase(), this);
+
+        return cf.toUpperCase();
+    };
+
     Chance.prototype.pl_pesel = function () {
         var number = this.natural({min: 1, max: 9999999999});
         var arr = this.pad(number, 10).split('');
