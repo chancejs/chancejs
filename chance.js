90cbbbbda81b33da96a34ebf298ffaa5aa504617
Harold Howe <howeharold@johndeere.com>
2018-02-27 10:44:03 -0600

    Improves shuffle performance. Fixes #352

--- chance.js
+++ chance.js
@@ -80,6 +80,10 @@
         return options;
     }
 
+    function range(size) {
+        return Array.apply(null, Array(size)).map(function (_, i) {return i;});
+    }
+
     function testRange(test, errorMessage) {
         if (test) {
             throw new RangeError(errorMessage);
@@ -462,18 +466,24 @@
     };
 
     Chance.prototype.shuffle = function (arr) {
-        var old_array = arr.slice(0),
-            new_array = [],
+        var new_array = [],
             j = 0,
-            length = Number(old_array.length);
+            length = Number(arr.length),
+            source_indexes = range(length),
+            last_source_index = length - 1,
+            selected_source_index;
 
         for (var i = 0; i < length; i++) {
             // Pick a random index from the array
-            j = this.natural({max: old_array.length - 1});
+            selected_source_index = this.natural({max: last_source_index});
+            j = source_indexes[selected_source_index];
+
             // Add it to the new array
-            new_array[i] = old_array[j];
-            // Remove that element from the original array
-            old_array.splice(j, 1);
+            new_array[i] = arr[j];
+
+            // Mark the source index as used
+            source_indexes[selected_source_index] = source_indexes[last_source_index];
+            last_source_index -= 1;
         }
 
         return new_array;
