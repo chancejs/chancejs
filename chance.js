b4f03aac5d5f16a07ae04a13c02ed20a2d510bc9
Victor Quinn <mail@victorquinn.com>
2018-04-25 16:55:13 -0400

    Bump version to include #361 and fix #362 which broke ES5 compatibility

--- chance.js
+++ chance.js
@@ -1,4 +1,4 @@
-//  Chance.js 1.0.14
+//  Chance.js 1.0.15
 //  http://chancejs.com
 //  (c) 2013 Victor Quinn
 //  Chance may be freely distributed or modified under the MIT license.
@@ -72,7 +72,7 @@
         return this;
     }
 
-    Chance.prototype.VERSION = "1.0.14";
+    Chance.prototype.VERSION = "1.0.15";
 
     // Random helper functions
     function initOptions(options, defaults) {
