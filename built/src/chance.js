48989b183e5693644bd87f22dd326be34c392fe6
Victor Quinn <mail@victorquinn.com>
2019-05-28 11:18:36 -0400

    Initial commit with lerna, typescript configs working

--- built/src/chance.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function fun() {
-    return true;
-}
-exports.fun = fun;
-//# sourceMappingURL=chance.js.map
--- built/tests/chance.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ava_1 = require("ava");
-ava_1.default('true is true', function (t) {
-    t.pass();
-});
-//# sourceMappingURL=chance.js.map
