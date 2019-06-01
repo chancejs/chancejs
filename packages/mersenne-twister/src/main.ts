b72345c55e4723a2fdc9056d2dd76e38cbb8767e
Victor Quinn <mail@victorquinn.com>
2019-05-31 22:23:04 -0400

    Fix lint, make things in mersenne twister private

--- packages/mersenne-twister/src/main.ts
+++ packages/mersenne-twister/src/main.ts
@@ -45,13 +45,13 @@
 */
 
 export default class MersenneTwister {
-    public N: number
-    public M: number
-    public MATRIX_A: number
-    public UPPER_MASK: number
-    public LOWER_MASK: number
-    public mt: any[]
-    public mti: number
+    private N: number
+    private M: number
+    private MATRIX_A: number
+    private UPPER_MASK: number
+    private LOWER_MASK: number
+    private mt: any[]
+    private mti: number
 
     constructor(seed: number) {
         if (seed === undefined) {
@@ -69,53 +69,6 @@ export default class MersenneTwister {
         this.mti = this.N + 1 /* mti==N + 1 means mt[N] is not initialized */
 
         this.init_genrand(seed)
-    }
-
-    /* initializes mt[N] with a seed */
-    public init_genrand(s: number) {
-        this.mt[0] = s >>> 0
-        for (this.mti = 1; this.mti < this.N; this.mti++) {
-            s = this.mt[this.mti - 1] ^ (this.mt[this.mti - 1] >>> 30)
-            this.mt[this.mti] = (((((s & 0xffff0000) >>> 16) * 1812433253) << 16) + (s & 0x0000ffff) * 1812433253) + this.mti
-            /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
-            /* In the previous versions, MSBs of the seed affect   */
-            /* only MSBs of the array mt[].                        */
-            /* 2002/01/09 modified by Makoto Matsumoto             */
-            this.mt[this.mti] >>>= 0
-            /* for >32 bit machines */
-        }
-    }
-
-    /* initialize by an array with array-length */
-    /* init_key is the array for initializing keys */
-    /* key_length is its length */
-    /* slight change for C++, 2004/2/26 */
-    public init_by_array (init_key: number[], key_length: number) {
-        let i = 1
-        let j = 0
-        let k
-        let s
-
-        this.init_genrand(19650218)
-        k = (this.N > key_length ? this.N : key_length)
-        for (; k; k--) {
-            s = this.mt[i - 1] ^ (this.mt[i - 1] >>> 30)
-            this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1664525) << 16) + ((s & 0x0000ffff) * 1664525))) + init_key[j] + j /* non linear */
-            this.mt[i] >>>= 0 /* for WORDSIZE > 32 machines */
-            i++
-            j++
-            if (i >= this.N) { this.mt[0] = this.mt[this.N - 1]; i = 1 }
-            if (j >= key_length) { j = 0 }
-        }
-        for (k = this.N - 1; k; k--) {
-            s = this.mt[i - 1] ^ (this.mt[i - 1] >>> 30)
-            this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1566083941) << 16) + (s & 0x0000ffff) * 1566083941)) - i /* non linear */
-            this.mt[i] >>>= 0 /* for WORDSIZE > 32 machines */
-            i++
-            if (i >= this.N) { this.mt[0] = this.mt[this.N - 1]; i = 1 }
-        }
-
-        this.mt[0] = 0x80000000 /* MSB is 1; assuring non-zero initial array */
     }
 
     /* generates a random number on [0,0xffffffff]-interval */
@@ -156,32 +109,79 @@ export default class MersenneTwister {
     }
 
     /* generates a random number on [0,0x7fffffff]-interval */
-    public genrand_int31(): number {
+    public genrand_int31 = (): number => {
         return (this.genrand_int32() >>> 1)
     }
 
     /* generates a random number on [0,1]-real-interval */
-    public genrand_real1(): number {
+    public genrand_real1 = (): number => {
         return this.genrand_int32() * (1.0 / 4294967295.0)
         /* divided by 2^32-1 */
     }
 
     /* generates a random number on [0,1)-real-interval */
-    public random(): number {
+    public random = (): number => {
         return this.genrand_int32() * (1.0 / 4294967296.0)
         /* divided by 2^32 */
     }
 
     /* generates a random number on (0,1)-real-interval */
-    public genrand_real3(): number {
+    public genrand_real3 = (): number => {
         return (this.genrand_int32() + 0.5) * (1.0 / 4294967296.0)
         /* divided by 2^32 */
     }
 
     /* generates a random number on [0,1) with 53-bit resolution*/
-    public genrand_res53(): number {
+    public genrand_res53 = (): number => {
         const a = this.genrand_int32()>>>5
         const b = this.genrand_int32()>>>6
         return (a * 67108864.0 + b) * (1.0 / 9007199254740992.0)
+    }
+
+    /* initializes mt[N] with a seed */
+    private init_genrand = (s: number) => {
+        this.mt[0] = s >>> 0
+        for (this.mti = 1; this.mti < this.N; this.mti++) {
+            s = this.mt[this.mti - 1] ^ (this.mt[this.mti - 1] >>> 30)
+            this.mt[this.mti] = (((((s & 0xffff0000) >>> 16) * 1812433253) << 16) + (s & 0x0000ffff) * 1812433253) + this.mti
+            /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+            /* In the previous versions, MSBs of the seed affect   */
+            /* only MSBs of the array mt[].                        */
+            /* 2002/01/09 modified by Makoto Matsumoto             */
+            this.mt[this.mti] >>>= 0
+            /* for >32 bit machines */
+        }
+    }
+
+    /* initialize by an array with array-length */
+    /* init_key is the array for initializing keys */
+    /* key_length is its length */
+    /* slight change for C++, 2004/2/26 */
+    private init_by_array = (init_key: number[], key_length: number) => {
+        let i = 1
+        let j = 0
+        let k
+        let s
+
+        this.init_genrand(19650218)
+        k = (this.N > key_length ? this.N : key_length)
+        for (; k; k--) {
+            s = this.mt[i - 1] ^ (this.mt[i - 1] >>> 30)
+            this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1664525) << 16) + ((s & 0x0000ffff) * 1664525))) + init_key[j] + j /* non linear */
+            this.mt[i] >>>= 0 /* for WORDSIZE > 32 machines */
+            i++
+            j++
+            if (i >= this.N) { this.mt[0] = this.mt[this.N - 1]; i = 1 }
+            if (j >= key_length) { j = 0 }
+        }
+        for (k = this.N - 1; k; k--) {
+            s = this.mt[i - 1] ^ (this.mt[i - 1] >>> 30)
+            this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1566083941) << 16) + (s & 0x0000ffff) * 1566083941)) - i /* non linear */
+            this.mt[i] >>>= 0
+            i++
+            if (i >= this.N) { this.mt[0] = this.mt[this.N - 1]; i = 1 }
+        }
+
+        this.mt[0] = 0x80000000 /* MSB is 1; assuring non-zero initial array */
     }
 }
