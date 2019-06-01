7316c425436d10cf25400cf4c5d6cb1a91bd233f
Victor Quinn <mail@victorquinn.com>
2019-05-31 21:55:38 -0400

    Add type annotations to returns on mersenne twister

--- packages/mersenne-twister/src/main.ts
+++ packages/mersenne-twister/src/main.ts
@@ -56,7 +56,7 @@ export default class MersenneTwister {
     constructor(seed: number) {
         if (seed === undefined) {
             // kept random number same size as time used previously to ensure no unexpected results downstream
-            seed = Math.floor(Math.random()*Math.pow(10,13))
+            seed = Math.floor(Math.random() * Math.pow(10,13))
         }
         /* Period parameters */
         this.N = 624
@@ -119,7 +119,7 @@ export default class MersenneTwister {
     }
 
     /* generates a random number on [0,0xffffffff]-interval */
-    public genrand_int32() {
+    public genrand_int32(): number {
         let y
         const mag01 = new Array(0x0, this.MATRIX_A)
         /* mag01[x] = x * MATRIX_A  for x=0,1 */
@@ -156,30 +156,30 @@ export default class MersenneTwister {
     }
 
     /* generates a random number on [0,0x7fffffff]-interval */
-    public genrand_int31() {
+    public genrand_int31(): number {
         return (this.genrand_int32() >>> 1)
     }
 
     /* generates a random number on [0,1]-real-interval */
-    public genrand_real1() {
+    public genrand_real1(): number {
         return this.genrand_int32() * (1.0 / 4294967295.0)
         /* divided by 2^32-1 */
     }
 
     /* generates a random number on [0,1)-real-interval */
-    public random() {
+    public random(): number {
         return this.genrand_int32() * (1.0 / 4294967296.0)
         /* divided by 2^32 */
     }
 
     /* generates a random number on (0,1)-real-interval */
-    public genrand_real3() {
+    public genrand_real3(): number {
         return (this.genrand_int32() + 0.5) * (1.0 / 4294967296.0)
         /* divided by 2^32 */
     }
 
     /* generates a random number on [0,1) with 53-bit resolution*/
-    public genrand_res53() {
+    public genrand_res53(): number {
         const a = this.genrand_int32()>>>5
         const b = this.genrand_int32()>>>6
         return (a * 67108864.0 + b) * (1.0 / 9007199254740992.0)
