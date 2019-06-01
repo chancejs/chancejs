5dd24d28e35b8982c3ccc8b7ffdd5327ef67b4ed
Victor Quinn <mail@victorquinn.com>
2019-05-31 22:53:22 -0400

    Add bool package

--- packages/core/src/main.test.ts
+++ packages/core/src/main.test.ts
@@ -3,51 +3,12 @@ import Chance from './main'
 
 const chance = new Chance()
 
-test('bool() returns a random boolean', t => {
-    const bool = chance.bool()
-    t.is(typeof bool, 'boolean')
+test('chance() returns a random number', t => {
+    t.is(typeof chance.random(), 'number')
 })
 
-test('bool() is within the bounds of what we would call random', t => {
-    let trueCount = 0
-    for (let i=0; i < 1000; i++) {
-        if (chance.bool()) {
-            trueCount++
-        }
+test('chance() returns a random number between 0 and 1', t => {
+    for (let i = 0; i < 1000; i++) {
+	t.true(chance.random() >= 0 && chance.random() <= 1)
     }
-
-    // The probability of this test failing is approximately 4.09e-86.
-    // So, in theory, it could give a false negative, but the sun will
-    // probably die long before that happens.
-
-    t.true((trueCount > 200) && (trueCount < 800))
-})
-
-test('bool() takes and obeys likelihood', t => {
-    let trueCount = 0
-    for (let i=0; i < 1000; i++) {
-        if (chance.bool({ likelihood: 30 })) {
-            trueCount++
-        }
-    }
-
-    // Expect it to average around 300
-    t.true((trueCount > 200) && (trueCount < 400))
-
-    trueCount = 0
-    for (let i=0; i < 1000; i++) {
-        if (chance.bool({ likelihood: 99 })) {
-            trueCount++
-        }
-    }
-
-    // Expect it to average at 990
-    t.true(trueCount > 900)
-})
-
-test('bool() throws an error if likelihood < 0 or > 100', t => {
-    const fn1 = () => chance.bool({likelihood: -23})
-    t.throws(fn1, RangeError)
-    const fn2 = () => chance.bool({likelihood: 7933})
-    t.throws(fn2, RangeError)
 })
--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -5,33 +5,10 @@ export interface IBoolOptions {
 }
 
 export default class Core {
-    private random: () => number
+    public random: () => number
 
     constructor() {
         const mt = new MersenneTwister(21)
         this.random = mt.random
-    }
-
-    /**
-     *  Return a random bool, either true or false
-     *
-     *  @param {Object} [options={ likelihood: 50 }] alter the likelihood of
-     *    receiving a true or false value back.
-     *  @throws {RangeError} if the likelihood is out of bounds
-     *  @returns {Bool} either true or false
-     */
-    public bool(options?: IBoolOptions): boolean {
-        let likelihood: number
-        if (options === undefined) {
-            likelihood = 50
-        } else {
-            likelihood = options.likelihood
-        }
-
-	if (likelihood < 0 || likelihood > 100) {
-	    throw new RangeError('Chance: Likelihood accepts values from 0 to 100.')
-	}
-
-        return this.random() * 100 < likelihood
     }
 }
