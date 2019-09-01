6f6aa18257d1bd6b68455ce6155d0037d36dfa96
Victor Quinn <mail@victorquinn.com>
2019-08-31 22:45:15 -0400

    Remove a lot of no longer used code

--- packages/integer/src/main.ts
+++ packages/integer/src/main.ts
@@ -2,6 +2,8 @@ import { Core } from '@chancejs/core'
 
 const core = new Core()
 
+// 9007199254740992 (2^53) is the max integer number in JavaScript
+// See: http://vq.io/132sa2j
 const MAX_INT = 9007199254740992
 const MIN_INT = -MAX_INT
 
