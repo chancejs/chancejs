55b16e32f1510fa76955c0d0ef736c4b61b056df
Paarth <paarthk+gh@gmail.com>
2020-05-28 19:08:32 -0400

    Added falsy generator

---
+++ packages/falsy/README.md
@@ -0,0 +1,5 @@
+# Falsy
+
+This is the `falsy` generator, to select from a set of falsy values (`[false, null, undefined, 0, NaN, '']`)
+
+Note: This is a WIP proof of concept. DO NOT USE YET. I will remove this once it's ready for prime time :)
---
+++ packages/falsy/src/main.ts
@@ -0,0 +1,27 @@
+import { Core } from '@chancejs/core'
+import { integer } from '@chancejs/integer';
+
+/**
+ * Type of default pool. `0 | NaN` collapses to just `number`, but other literals persist.
+ */
+type FalsyValue = false | null | undefined | number | '';
+
+export interface IFalsyOptions {
+    pool: FalsyValue[];
+}
+
+const chance = new Core()
+
+/**
+ *  Return a random falsy value (false, null, undefined, 0, NaN, '').
+ *
+ *  @param {Object} [options={ pool: (false | null | undefined | number | '')[] }] alter the pool
+ *      of possible values.
+ *  @returns {FalsyValue} One of [false, null, undefined, 0, NaN, ''], or of the pool provided.
+ */
+export function falsy(options?: IFalsyOptions): FalsyValue {
+    const {pool} = {...options, pool: [false, null, undefined, 0, NaN, ''] as const};
+    const value = pool[integer(0, pool.length - 1)];
+
+    return value;
+}
