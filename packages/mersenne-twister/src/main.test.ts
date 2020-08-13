5bda47b7ea6f6cb10777149a25c638d1c5dfd5f6
Enrique Lozano <enriquelozano@Enriques-iMac.local>
2020-08-13 18:03:02 -0400

    Add clarity to MarsenneTwister test

--- packages/mersenne-twister/src/main.test.ts
+++ packages/mersenne-twister/src/main.test.ts
@@ -1,7 +1,7 @@
 import { MersenneTwister } from './main'
 import test from 'ava'
 
-test("test", (t) => {
+test("MarsenneTwister returns a random number between 0 and 1", (t) => {
   const twister = new MersenneTwister(21)
   const random = twister.random()
   t.true(random > 0)
