827e8354935a38fe6fa3a89af617f344d4673f24
Victor Quinn <mail@victorquinn.com>
2021-09-01 16:31:21 -0400

    Bump version to 1.1.8

--- chance.js
+++ chance.js
@@ -1,4 +1,4 @@
-//  Chance.js 1.1.7
+//  Chance.js 1.1.8
 //  http://chancejs.com
 //  (c) 2013 Victor Quinn
 //  Chance may be freely distributed or modified under the MIT license.
@@ -72,7 +72,7 @@
         return this;
     }
 
-    Chance.prototype.VERSION = "1.1.7";
+    Chance.prototype.VERSION = "1.1.8";
 
     // Random helper functions
     function initOptions(options, defaults) {
