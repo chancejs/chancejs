3a3b1640e4421b933ae1d660c1b615ffc69ddfc2
Abdou <2000abdallah2014@gmail.com>
2023-09-01 12:34:05 +0100

    added nin generator

--- chance.js
+++ chance.js
@@ -1276,7 +1276,20 @@
      Chance.prototype.zodiac = function () {
         const zodiacSymbols = ["Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn","Aquarius","Pisces"];
         return this.pickone(zodiacSymbols);
-    };
+    }; 
+    // Generate random  National Identification Number (NIN)
+    Chance.prototype.nin = function(options){
+       options = initOptions(options, {nationality:'us',})
+    
+    let nin ;
+    if(options.nationality === ("us"|| "uk")) nin = this.string({ pool: '0123456789', length: 9})
+    else if(options.nationality ===("br" ||"gr")) nin = this.string({ pool: '0123456789', length: 11})
+    else if(options.nationality ===("jp" ||"ca")) nin = this.string({ pool: '0123456789', length: 12})
+    else if(options.nationality ==="ru") nin = this.string({ pool: '0123456789', length: 14})
+    else if(options.nationality ==="it") nin = this.string({ pool: '0123456789', length: 16})
+    else if(options.nationality ==="ch") nin = this.string({ pool: '0123456789', length: 18})
+    return nin
+    }
 
 
     // -- End Person --
