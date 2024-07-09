f915240410f7c5ee2f22b7960081ac39c81ec411
Victor Quinn <mail@victorquinn.com>
2024-07-09 11:00:18 -0400

    Bump version to 1.1.12 to include emoji generator

--- chance.js
+++ chance.js
@@ -1,4 +1,4 @@
-//  Chance.js 1.1.11
+//  Chance.js 1.1.12
 //  https://chancejs.com
 //  (c) 2013 Victor Quinn
 //  Chance may be freely distributed or modified under the MIT license.
@@ -72,7 +72,7 @@
         return this;
     }
 
-    Chance.prototype.VERSION = "1.1.11";
+    Chance.prototype.VERSION = "1.1.12";
 
     // Random helper functions
     function initOptions(options, defaults) {
